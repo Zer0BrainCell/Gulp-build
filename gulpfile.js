f369e7345c8e8b23b7b1873c85841b1f146b1d92
Zer0BrainCell <zenya9415@gmail.com>
2025-07-05 21:20:04 +0300

    с2

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const rename = require('gulp-rename')
 const cleanCss = require('gulp-clean-css')
 const babel = require('gulp-babel') 
 const uglify = require('gulp-uglify') 
-const concat = require('gulp-concat') 
+const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
 // Константа с путями изначальных файлов и файлов назначения
 const paths = {
     styles:{
@@ -16,6 +19,11 @@ const paths = {
         src: 'src/scripts/**/*.js',
         dest: 'dist/js/'
     },
+    images : {
+        src: 'src/img/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'dist/img/'
+
+    }
 }
 // Функция для очистки dist
 function clean(){
@@ -24,21 +32,38 @@ function clean(){
 // Функция для обработки стилей
 function styles(){
     return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(cleanCss())
+    .pipe(autoprefixer({
+			cascade: false
+		}))
+    .pipe(cleanCss(
+        {level: 2}
+    ))
     .pipe(rename(
         {basename:'main',
          suffix : '.min'
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.dest))
 }
 // Функция для обработки скриптов
 function scripts(){
-    return gulp.src(paths.scripts.src, {sourcemaps: true})
-    .pipe(babel())
+    return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+         presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.scripts.dest))
+}
+// Минификация изображений
+function img(){
+    return gulp.src(paths.images.src, { encoding: false })
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images.dest))
 }
 // Функция для наблюдения за каталогом стилей
 function watch(){
@@ -46,11 +71,12 @@ function watch(){
     gulp.watch(paths.scripts.src,scripts )
 }
 // Функция для запуска билда сборки стилей
-const build = gulp.series(clean,gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean,gulp.parallel(styles, scripts, img), watch)
 
 exports.clean = clean
 exports.styles = styles
 exports.scripts = scripts
+exports.img = img
 exports.watch = watch
 exports.build = build
 exports.default = build
